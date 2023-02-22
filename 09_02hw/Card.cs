e5ab5d33c0c3b5ccb396d8952bf8c2b98484bdcf
Sheogorattio <no.passion.there.is.serenity@gmail.com>
2023-02-23 00:46:11 +0200

    Add project files.

---
+++ 09_02hw/Card.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CardClass
+{
+    public class Card
+    {
+        public Card(string name, int pin, DateTime expirationDate,int currentMoneyValue)
+        {
+            Name = name;
+            Pin = pin;
+            ExpirationDate = expirationDate;
+            CurrentMoneyValue = currentMoneyValue;
+            CurrentCreditLimit = 20000;
+        }
+        public string Name { get; set; }
+        int Pin { get; set; }
+        public DateTime ExpirationDate { get; set; }
+        int CurrentCreditLimit{get;set;}
+        int CurrentMoneyValue { get; set; }
+        public void ReplenishAccount(int value)
+        {
+            CurrentMoneyValue+=value;
+        }
+        public void SpendMoney(int value)
+        {
+            if (CurrentMoneyValue >= value) CurrentMoneyValue -= value;
+            else throw new ArgumentException("CurrentMoneyValue <= value");
+        }
+        public void SpendCreditMoney(int value)
+        {
+            if(CurrentCreditLimit >= value) CurrentCreditLimit -= value;
+            else throw new ArgumentException("CurrentCreditLimit <= value");
+        }
+        public void EditPin(int value)
+        {
+            Pin = value;
+        }
+    }
+
+    public delegate void CardDelegateVoid(int value);
+
+    public class SourceEventInt
+    {
+       public CardDelegateVoid ev;
+       public void GenerateEvent(int value)
+        {
+            ev.Invoke(value);
+        }
+    }
+
+}
---
+++ 09_02hw/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CardClass;
+
+
+namespace _09_02hw
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                SourceEventInt addMoney = new SourceEventInt();
+                SourceEventInt spendMoney = new SourceEventInt();
+                SourceEventInt editPin = new SourceEventInt();
+                Card card = new Card("Name", 9123, new DateTime(2025, 2, 12), 3000);
+
+                addMoney.ev += card.ReplenishAccount;
+                spendMoney.ev += card.SpendMoney;
+                editPin.ev += card.EditPin;
+
+                addMoney.ev.Invoke(2000);
+                spendMoney.ev.Invoke(5000);
+
+                Console.ReadKey();
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.ReadKey();
+                Environment.Exit(1);
+            }
+            
+        }
+
+    }
+}
